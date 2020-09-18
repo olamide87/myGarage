6b6e4aa49efbcb490d4db00da55896f0a31bebb4
Olamide Oladinni <olajide1987@gmail.com>
2020-09-17 23:05:36 -0500

    login works

---
+++ src/components/BoardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <div className="BoardContainer">
+        <h2>BoardContainer</h2>
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import LogOut from './LogOut';
+import Auth from './Auth';
+
+class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: propTypes.bool,
+  }
+
+  render() {
+    const { authed } = this.props;
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <p className="navbar-brand">Navbar</p>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <p className="nav-link">Home <span className="sr-only">(current)</span></p>
+            </li>
+          </ul>
+        </div>
+        {authed && <LogOut />}
+        {authed || <Auth />}
+      </nav>
+    );
+  }
+}
+
+export default MyNavbar;
